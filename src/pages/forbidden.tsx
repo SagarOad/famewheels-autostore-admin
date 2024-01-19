a6ec1f46b82536d2241d91909edc3187b5487074
Muhammad Mehroz <97299789+mmehroz@users.noreply.github.com>
2024-01-19 12:54:59 +0000

    Delete src directory

--- src/pages/forbidden.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import PrivateRoute from "../route/PrivateRoute";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <h1>You are not allowed.</h1>;
-}
