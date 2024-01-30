938726042219e1389e7ccb66a9dff303ada8b149
NomanGhayyur <nomanghayyur96@gmail.com>
2024-01-30 18:42:43 +0500

    fixed image carousel

--- src/Components/BonusUi/OwlCarousel/WithIndicators.tsx
+++ src/Components/BonusUi/OwlCarousel/WithIndicators.tsx
@@ -1,16 +1,25 @@
 import CommonCardHeader from "@/CommonComponent/CommonCardHeader";
 import { WithIndicator } from "@/Constant";
-import { WithIndicatorData, WithIndicatorDataList } from "@/Data/BonusUi/OwlCarousel";
+import {
+  WithIndicatorData,
+  WithIndicatorDataList,
+} from "@/Data/BonusUi/OwlCarousel";
 import { Card, CardBody, Col } from "reactstrap";
 import CommonCarousel from "./Common/CommonCarousel";
 
-const WithIndicators = ({images,id}:{images:any,id:any}) => {
+const WithIndicators = ({ images, id }: { images: any; id: string }) => {
   return (
     <Col>
       <Card>
         {/* <CommonCardHeader title={WithIndicator} span={WithIndicatorData} /> */}
         <CardBody className="bg-dark rounded p-2">
-          <CommonCarousel id={id} images={images} data={WithIndicatorDataList} control indecators ride="carousel" />
+          <CommonCarousel
+            token={id}
+            images={images}
+            control
+            indecators
+            ride="carousel"
+          />
         </CardBody>
       </Card>
     </Col>
