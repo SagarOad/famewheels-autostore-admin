1ae977f933e6e994f48230ad0376b83cc9b57b40
NomanGhayyur <nomanghayyur96@gmail.com>
2024-01-31 20:56:48 +0500

    added user API

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -5,17 +5,45 @@ import { PageHeader } from "./PageHeader";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/Redux/Hooks";
 import { headerResponsive } from "@/Redux/Reducers/LayoutSlice";
+import axios from "axios";
+import { setUser } from "@/Redux/Reducers/UserSlice";
 
 export const Header = () => {
   const { toggleSidebar } = useAppSelector((state) => state.layout);
   const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.user);
+
+  const formSubmitHandle = async () => {
+    const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+    try {
+      const token = localStorage.getItem("authToken");
+
+      const response = await axios.get(`${BASE_URL}/getUser`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      dispatch(setUser(response?.data));
+    } catch (error: any) {
+      console.error("Failed to get User", error);
+    }
+  };
 
   useEffect(() => {
+    if (user?.name === "" && user?.email === "" && user?.role === "") {
+      formSubmitHandle();
+    }
+
     dispatch(headerResponsive());
   }, []);
 
   return (
-    <Row className={`page-header ${toggleSidebar ? "close_icon" : ""}`} id="page-header">
+    <Row
+      className={`page-header ${toggleSidebar ? "close_icon" : ""}`}
+      id="page-header"
+    >
       <MobileView />
       <BreadCrumbs />
       <PageHeader />
