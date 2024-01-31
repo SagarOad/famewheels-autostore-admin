98a5b21f900ada4c64636ec2c8e14ca415771e8c
NomanGhayyur <nomanghayyur96@gmail.com>
2024-01-31 20:57:29 +0500

    created userslice

---
+++ src/Redux/Reducers/UserSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    name: "",
+    email: "",
+    role: "",
+  },
+};
+
+const UserSlice = createSlice({
+  name: "UserSlice",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setUser } = UserSlice.actions;
+
+export default UserSlice.reducer;
