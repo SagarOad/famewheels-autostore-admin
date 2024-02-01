826ed738797fa510c72a80faabb3ce9719452e54
NomanGhayyur <nomanghayyur96@gmail.com>
2024-02-01 17:53:41 +0500

    removed consoles

--- src/app/[lng]/(MainBody)/inspection/inspectionlist/page.tsx
+++ src/app/[lng]/(MainBody)/inspection/inspectionlist/page.tsx
@@ -52,7 +52,6 @@ const AllInspection = () => {
       });
       // setPage(response?.data?.data?.current_page);
       setTotal(response?.data?.data?.last_page);
-      console.log(response?.data);
       return response?.data?.data?.data;
     } catch (error) {
       console.log(error);
