5bb2af87531e42f29fec6b4cf240aba7a64ebf07
NomanGhayyur <nomanghayyur96@gmail.com>
2024-02-01 16:47:23 +0500

    working on dashboard

--- src/Components/Dashboard/DefaultDashboard/page.tsx
+++ src/Components/Dashboard/DefaultDashboard/page.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Container, Row } from "reactstrap";
+"use client";
+
+import React, { useState } from "react";
+import { Card, Col, Container, Input, Row } from "reactstrap";
 import UserInfo from "./UserInfo";
 import OpeningOfLeaflets from "./OpeningOfLeaflets";
 import ShiftsOverview from "./ShiftsOverview";
@@ -17,34 +19,50 @@ import StudyStatistics from "../Education/StudyStatistics";
 const DefaultDashboardContainer = () => {
   return (
     <>
-    <Container fluid className="default-dashboard">
-      <Row className="widget-grid">
-        <UserInfo />
-        <OpeningOfLeaflets />
-        <ShiftsOverview />
-      </Row>
-    </Container>
-    <Container fluid className="dashboard-4">
-
+      <Container fluid className="default-dashboard">
+        <Card className="profile-greeting p-2">
+          <Row className="widget-grid">
+            <Col xxl="10" md="5" className="proorder-xl-3 proorder-md-3"></Col>
+            <Col xxl="2" className="box-col-12 proorder-xl-8 proorder-md-9">
+              <Input className="digits" type="month" defaultValue="2023-01" />
+            </Col>
+          </Row>
+        </Card>
+      </Container>
+      <Container fluid className="default-dashboard">
+        <Row className="widget-grid">
+          <UserInfo />
+          <OpeningOfLeaflets />
+          <ShiftsOverview />
+        </Row>
+      </Container>
+      <Container fluid className="dashboard-4">
         {/* <ProjectsTable /> */}
         {/* <CustomerTransaction /> */}
         {/* <UserNotifications /> */}
         {/* <UpcomingAppointments /> */}
         {/* <ActiveMembers /> */}
         <Row>
-<TotalStudents/>
-<StudyStatistics />
+          <TotalStudents />
+          <StudyStatistics />
         </Row>
-        {/* <SalesStatistic /> */}
         {/* <SalesByProduct /> */}
-    </Container>
+      </Container>
 
-    <Container fluid className="default-dashboard">
-      <Row className="widget-grid">
-     
-        <SalesStatistic />
-      </Row>
-    </Container>
+      <Container fluid className="default-dashboard">
+        <Row className="widget-grid">
+          <SalesStatistic />
+        </Row>
+      </Container>
+      <Container fluid className="dashboard-4">
+        <ProjectsTable />
+        <CustomerTransaction />
+        <UserNotifications />
+        <UpcomingAppointments />
+        <ActiveMembers />
+
+        <SalesByProduct />
+      </Container>
     </>
   );
 };
