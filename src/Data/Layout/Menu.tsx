24a80b84311307477a4aae564c4d63c59c1ea4a6
NomanGhayyur <nomanghayyur96@gmail.com>
2024-03-19 12:12:55 +0500

    added managed by famewheels

--- src/Data/Layout/Menu.tsx
+++ src/Data/Layout/Menu.tsx
@@ -16,7 +16,7 @@ export const MenuList: MenuItem[] | undefined = [
         path: "/dashboard/default_dashboard",
       },
     ],
-    role:[Admin]
+    role: [Admin],
   },
 
   {
@@ -32,38 +32,48 @@ export const MenuList: MenuItem[] | undefined = [
         lanClass: "lan-2",
         active: false,
         children: [
-          { path: "/users/management", title: "Management", type: "link", 
-            role:[Admin]
-        },
+          {
+            path: "/users/management",
+            title: "Management",
+            type: "link",
+            role: [Admin],
+          },
           { path: "/users/members", title: "Members", type: "link" },
-          { path: "/users/subscribers", title: "Subscribers", type: "link", 
-            role:[Admin]
-        },
+          {
+            path: "/users/subscribers",
+            title: "Subscribers",
+            type: "link",
+            role: [Admin],
+          },
           { path: "/users/dealers", title: "Dealers", type: "link" },
-          { path: "/users/inspectors", title: "Inspectors", type: "link", 
-            role:[Admin]
-        },
-          { path: "/users/userslist", title: "Users List", type: "link", 
-            role:[Admin]
-        },
+          {
+            path: "/users/inspectors",
+            title: "Inspectors",
+            type: "link",
+            role: [Admin],
+          },
+          {
+            path: "/users/userslist",
+            title: "Users List",
+            type: "link",
+            role: [Admin],
+          },
           {
             path: "/users/membersrequests",
             title: "Member Requests",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/users/rejected-members-requests",
             title: "Rejected Requests",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
-          
         ],
       },
-      
     ],
-    role:[Admin]
+    role: [Admin],
   },
 
   {
@@ -83,7 +93,7 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/inspection/inspectionlist",
             title: "Inspection List",
             type: "link",
-            role:[Admin,User]
+            role: [Admin, User],
           },
           // {
           //   path: "/inspection/pending_inspection",
@@ -103,8 +113,7 @@ export const MenuList: MenuItem[] | undefined = [
         ],
       },
     ],
-    role:[Admin,User]
-
+    role: [Admin, User],
   },
 
   {
@@ -124,25 +133,24 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/cartransfer/car_transferlist",
             title: "Car Transfer",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/cartransfer/car_import",
             title: "Car Import",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/cartransfer/car_registration",
             title: "Car Registration",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
 
   {
@@ -151,7 +159,7 @@ export const MenuList: MenuItem[] | undefined = [
     menucontent: "Dashboards,Widgets",
     Items: [
       {
-        title: "Website Posts",
+        title: "Normal Ads",
         id: 5,
         icon: "widget",
         type: "sub",
@@ -163,25 +171,53 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/posts/pending_posts",
             title: "Pending Posts",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/posts/rejected_posts",
             title: "Rejected Posts",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/posts/approved_posts",
             title: "Approved Posts",
             type: "link",
-            role:[Admin]
+            role: [Admin],
+          },
+        ],
+      },
+      {
+        title: "Managed by FameWheels",
+        id: 5,
+        icon: "widget",
+        type: "sub",
+        lanClass: "lan-6",
+        active: false,
+        children: [
+          { path: "/posts/all_posts", title: "All Posts", type: "link" },
+          {
+            path: "/managed_by_fameWheels/active_posts",
+            title: "Active Ads",
+            type: "link",
+            role: [Admin],
+          },
+          {
+            path: "/managed_by_fameWheels/sold_posts",
+            title: "Sold Ads",
+            type: "link",
+            role: [Admin],
+          },
+          {
+            path: "/managed_by_fameWheels/all_posts",
+            title: "All Ads",
+            type: "link",
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
 
   {
@@ -201,25 +237,24 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/refund/pending_refund",
             title: "Pending Refund",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/refund/rejected_refund",
             title: "Rejected Refund",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/refund/approved_refund",
             title: "Approved Refund",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
 
   {
@@ -239,25 +274,24 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/payments/active_payments",
             title: "Active Payments",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/payments/wallet_payments",
             title: "Wallet Payments",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/payments/used_payments",
             title: "Used Payments",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
 
   {
@@ -277,55 +311,54 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/bidding/pending_request",
             title: "Pending Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/rejected_request",
             title: "Rejected Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/forwarded_request",
             title: "Forwarded Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/inprocess_request",
-            title: "Inprocess Request",
+            title: "Inspection Started",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/inspected_request",
             title: "Inspected Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/sale_through_famewheels",
             title: "Sale Through Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/eligible",
             title: "Eligible Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/bidding/not_eligible",
             title: "Not Eligible Request",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
 
   {
@@ -345,21 +378,19 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/new_car/carpost",
             title: "Ad New Vehicles",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/new_car/carpostlist",
             title: "New Vehicles List",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
-
 
   {
     title: "Auction",
@@ -378,16 +409,13 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/auction/auction_cars",
             title: "Auction Cars List",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
-          
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
-
 
   {
     title: "Dealer",
@@ -406,21 +434,17 @@ export const MenuList: MenuItem[] | undefined = [
             path: "/dealer/add_dealer",
             title: "Add Dealer",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
           {
             path: "/dealer/dealerslist",
             title: "Dealers List",
             type: "link",
-            role:[Admin]
+            role: [Admin],
           },
-          
         ],
       },
     ],
-    role:[Admin,Hr]
-
+    role: [Admin, Hr],
   },
-
-
 ];
